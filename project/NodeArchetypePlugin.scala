94d22599ee0854cfd57dccaea8b8ae9059f85cf7
Nepomuk Seiler <nepomuk.seiler@gutefrage.net>
2017-01-04 11:10:52 +0100

    Use a custom target folder

--- project/NodeArchetypePlugin.scala
+++ project/NodeArchetypePlugin.scala
@@ -1,7 +1,7 @@
 package net.gutefrage.node
 
 import com.typesafe.sbt.SbtNativePackager.Universal
-import com.typesafe.sbt.packager.Keys.executableScriptName
+import com.typesafe.sbt.packager.Keys.{executableScriptName, stage}
 import com.typesafe.sbt.packager.MappingsHelper
 import com.typesafe.sbt.packager.universal.UniversalPlugin
 import org.scalajs.core.tools.io.{FileVirtualJSFile, VirtualJSFile}
@@ -9,6 +9,7 @@ import sbt._
 import sbt.Keys._
 import org.scalajs.sbtplugin.ScalaJSPlugin.autoImport._
 
+import scalajsbundler.{NpmDependencies, PackageJson}
 import scalajsbundler.sbtplugin.ScalaJSBundlerPlugin
 import scalajsbundler.sbtplugin.ScalaJSBundlerPlugin.autoImport._
 import scalajsbundler.util.JS
@@ -27,16 +28,19 @@ object NodeArchetypePlugin extends AutoPlugin {
   override lazy val requires: Plugins = ScalaJSBundlerPlugin && UniversalPlugin
 
   override def projectSettings: Seq[Setting[_]] = Seq(
+    (crossTarget in packageBin in (Compile, npmUpdate)) := crossTarget.value / "scalajs-bundler" / "universal",
+//    scalaJSBundlerPackageJson in packageBin := createPackageJson(
+//      npmDependencies.value,
+//      npmResolutions.value,
+//      fullClasspath.value,
+//      streams.value.log
+//    ),
     scalaJSLauncher in packageBin := {
+      val targetDir = (crossTarget in packageBin in (Compile, npmUpdate)).value
       val scriptName = (executableScriptName in Universal).value
       val main = (mainClass in Compile in scalaJSLauncher in fullOptJS).value
         .getOrElse(sys.error("No main class detected"))
-      val launcher = writeLauncher(
-        scriptName,
-        (crossTarget in npmUpdate).value,
-        (fullOptJS in Compile).value,
-        main
-      )
+      val launcher = writeLauncher(scriptName, targetDir, (fullOptJS in Compile).value, main)
       Attributed[VirtualJSFile](FileVirtualJSFile(launcher))(
         AttributeMap.empty.put(name.key, main)
       )
@@ -80,5 +84,21 @@ object NodeArchetypePlugin extends AutoPlugin {
         .split('.')
         .foldLeft(module)((tree, part) => tree.bracket(part))
     mainClassRef.apply().dot("main").apply()
+  }
+
+  private def createPackageJson(targetDir: File,
+                                npmDependencies: Seq[(String, String)],
+                                npmResolutions: Map[String, String],
+                                fullClasspath: Seq[Attributed[File]],
+                                log: Logger): File = {
+    val dependencies = NpmDependencies.collectFromClasspath(fullClasspath).compileDependencies
+
+    val packageJson = JS.obj(
+      "dependencies" -> JS.objStr(PackageJson.resolveDependencies(dependencies, npmResolutions, log))
+    )
+    log.debug("Writing 'package.json'")
+    val targetFile = targetDir / "package.json"
+    IO.write(targetFile, JS.toJson(packageJson))
+    targetFile
   }
 }
